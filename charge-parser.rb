38cbfad55d87c958ef53f62b14f2f5fd16c73f91
Tyler Wymer <twymer@gmail.com>
2013-03-19 12:22:48 -0400

    Break up regex to make it readable

--- charge-parser.rb
+++ charge-parser.rb
@@ -5,7 +5,16 @@ require 'pry'
 
 original_input = "$4.99 TXT MESSAGING – 250 09/29 – 10/28 4.99"
 
-matcher = /^\$\d*\.\d{2}\s*(.*)\s*(\d{2}\/\d{2}\W.\W\d{2}\/\d{2})\s*(\d*\.\d{2})/
+price_matcher = /\d*\.\d{2}/
+date_matcher = /\d{2}\/\d{2}/
+
+matcher = /
+            ^\$#{price_matcher}                    # ignore first price
+            \s*(.*)\s*                             # everything between price and dates
+            (#{date_matcher}\W.\W#{date_matcher})  # date range
+            \s*
+            (#{price_matcher})                     # price
+          /x
 
 result = original_input.scan(matcher)
 puts result
